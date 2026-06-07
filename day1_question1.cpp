f13998a6d19d4fdf0784509f1debb7e9250514d6
unerring1708 <24121cn052@glbitm.ac.in>
2026-06-07 14:40:26 +0530

    QUESTION 1
    Practice question

---
+++ day1_question1.cpp
@@ -0,0 +1,20 @@
+//TWOSUM
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            int num=nums[i];
+            int complement=target-num;
+            if(mpp.find(complement)!=mpp.end())
+            {
+                return {mpp[complement],i};
+            }
+            mpp[num]=i;
+
+        }
+        return{-1,-1};
+    }
+};
